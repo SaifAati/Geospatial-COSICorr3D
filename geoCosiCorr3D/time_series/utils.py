d174a06868b62026edf7cf7ee36d82547bac649e
SaifAati <saif.aati@planet.com>
2024-11-11 18:19:52 -0800

    v.2.5.2: fix: update `median_absolute_deviation` to `median_abs_deviation` in `geoStat` class
    - Updated the import from `scipy.stats` to use `median_abs_deviation` instead of the deprecated `median_absolute_deviation`.
    - Adjusted the `geoStat` class to reflect this change.

--- geoCosiCorr3D/time_series/utils.py
+++ geoCosiCorr3D/time_series/utils.py
@@ -3,18 +3,7 @@
 # Contact: SAIF AATI  <saif@caltech.edu> <saifaati@gmail.com>
 # Copyright (C) 2024
 """
-import numpy as np
 
 
-def build_flatten_data_cube(data_cube: np.ndarray) -> np.ndarray:
-    """
-    Flattens a 3D data cube into a 2D array where each row corresponds to a flattened 2D slice of the cube.
-    Args:
-        data_cube: 3D array with shape (bands, height, width)
 
-    Returns:
-        2D array with shape (height * width, bands)
 
-    """
-    data_cube_shape = data_cube.shape
-    return data_cube.reshape(data_cube_shape[0], -1).T
